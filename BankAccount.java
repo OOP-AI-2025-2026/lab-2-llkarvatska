25d4c17c93c9d87c94c09da7193dd6cd0ce3e580
llkarvatska <llkarvatska@gmail.com>
2025-09-25 20:11:21 +0300

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,40 @@
+public class BankAccount {
+    String name;
+    double balance;
+    double transactionFee = 0;
+
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    double getBalance() {
+        return this.balance;
+    }
+
+    boolean withdraw(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        double total = amount + transactionFee;
+        if (balance >= total) {
+            balance -= total;
+            return true;
+        }
+        return false;
+    }
+
+    boolean transfer(BankAccount receiver, double amount) {
+        if (receiver == null || amount <= 0) {
+            return false;
+        }
+        double total = amount + transactionFee;
+        if (balance >= total) {
+            balance -= total;
+            receiver.balance += amount;
+            return true;
+        }
+        return false;
+    }
+}
